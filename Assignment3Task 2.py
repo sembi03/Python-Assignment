7d3949476960c884bbff2f8c2fd72753c9191aff
sembi03 <sembiyan24@gmail.com>
2025-05-06 12:35:58 +0530

    Add files via upload

---
+++ Assignment3Task 2.py
@@ -0,0 +1,12 @@
+import math
+
+def maths(x):
+    print('Square root:',math.sqrt(a))
+    print('Logarithm: ',math.log(a))
+    print('Sine: ',math.sin(a))
+
+a=int(input("Enter the number: "))
+maths(a)
+
+
+
